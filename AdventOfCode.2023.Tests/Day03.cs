7d9d4a2c48058f4be3324a9a6e819c3e6d52b7f7
karalabe <karalabenak@gmail.com>
2023-12-07 23:19:43 +0100

    Solution for day 03

---
+++ AdventOfCode.2023.Tests/Day03.cs
@@ -0,0 +1,53 @@
+﻿namespace AdventOfCode2023.Tests;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Collections.Generic;
+using System.IO;
+
+[TestClass]
+public class Day03
+{
+    static readonly IEnumerable<string> TestInput = File.ReadLines("Inputs/Day03.Example.txt");
+    static readonly IEnumerable<string> RealInput = File.ReadLines("Inputs/Day03.txt");
+
+    [TestMethod]
+    public void SolvePart1_ForWholeTestInput_ReturnsCorrectResult()
+    {
+        var input = TestInput;
+
+        var result = AdventOfCode2023.Day03.SolvePart1(input);
+
+        Assert.AreEqual("4361", result);
+    }
+
+    [TestMethod]
+    public void SolvePart1_ForWholeRealInput_ReturnsCorrectResult()
+    {
+        var input = RealInput;
+
+        var result = AdventOfCode2023.Day03.SolvePart1(input);
+
+        Assert.AreEqual("531932", result);
+    }
+
+    [TestMethod]
+    public void SolvePart2_ForWholeTestInput_ReturnsCorrectResult()
+    {
+        var input = TestInput;
+
+        var result = AdventOfCode2023.Day03.SolvePart2(input);
+
+        Assert.AreEqual("467835", result);
+    }
+
+    [TestMethod]
+    public void SolvePart2_ForWholeRealInput_ReturnsCorrectResult()
+    {
+        var input = RealInput;
+
+        var result = AdventOfCode2023.Day03.SolvePart2(input);
+
+        Assert.AreEqual("73646890", result);
+    }
+}
---
+++ AdventOfCode.2023/Day03.cs
@@ -0,0 +1,109 @@
+﻿namespace AdventOfCode2023;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public readonly record struct Symbol(int Line, int Char, char SymbolChar);
+public readonly record struct Number(int Line, int Char, int Length, int Value);
+
+public partial class Day03
+{
+    private static Regex SymbolRegex = GetSymbolRegex();
+    private static Regex NumberRegex = GetNumberRegex();
+
+    public static string? SolvePart1(IEnumerable<string> input) => GetPartNumbers(input).Select(n => n.Value).Sum().ToString();
+
+    public static string? SolvePart2(IEnumerable<string> input) => GetGearRatios(input).Sum().ToString();
+
+    public static IEnumerable<Number> GetPartNumbers(IEnumerable<string> input)
+    {
+        var symbolsFromPreviousLine = Enumerable.Empty<Symbol>();
+        var numbersFromPreviousLine = Enumerable.Empty<Number>();
+        var lineNum = 0;
+
+        foreach (var line in input)
+        {
+            var symbols = GetSymbols(lineNum, line);
+            var numbers = GetNumbers(lineNum++, line).ToList();
+
+            for (var i = 0; i < numbers.Count; ++i)
+            {
+                var number = numbers[i];
+
+                if (symbolsFromPreviousLine.Any(s => IsAdjacent(s, number))
+                    || symbols.Any(s => IsAdjacent(s, number)))
+                {
+                    yield return number;
+
+                    numbers.RemoveAt(i--);
+                }
+            }
+
+            var numbersP = numbersFromPreviousLine.ToList();
+
+            foreach (var symbol in symbols)
+            {
+                for (var i = 0; i < numbersP.Count; ++i)
+                {
+                    var number = numbersP[i];
+
+                    if (IsAdjacent(symbol, number))
+                    {
+                        yield return number;
+
+                        numbersP.RemoveAt(i--);
+                    }
+                }
+            }
+
+            symbolsFromPreviousLine = symbols;
+            numbersFromPreviousLine = numbers;
+        }
+    }
+
+    private static bool IsAdjacent(Symbol symbol, Number number)
+        => symbol.Char >= number.Char - 1 && symbol.Char <= number.Char + number.Length ||
+            symbol.Char == number.Char - 1 || symbol.Char == number.Char + number.Length ||
+            number.Char - 1 <= symbol.Char && number.Char + number.Length >= symbol.Char;
+
+    public static IEnumerable<long> GetGearRatios(IEnumerable<string> input)
+    {
+        var gearSymbolsFromPreviousLine = Enumerable.Empty<Symbol>();
+        var numbersFromPreviousLine = Enumerable.Empty<Number>();
+        var numbersFromPreviousLineBefore = Enumerable.Empty<Number>();
+        var lineNum = 0;
+
+        foreach (var line in input)
+        {
+            var gearSymbols = GetSymbols(lineNum, line).Where(s => s.SymbolChar == '*');
+            var numbers = GetNumbers(lineNum++, line);
+
+            foreach (var symbol in gearSymbolsFromPreviousLine)
+            {
+                var adj = GetAdjacentNumbers(symbol, numbersFromPreviousLineBefore.Concat(numbersFromPreviousLine).Concat(numbers)).ToArray();
+
+                if (adj.Length == 2) yield return (long)adj[0].Value * adj[1].Value;
+            }
+
+            numbersFromPreviousLineBefore = numbersFromPreviousLine;
+            numbersFromPreviousLine = numbers;
+            gearSymbolsFromPreviousLine = gearSymbols;
+        }
+    }
+
+    private static IEnumerable<Number> GetAdjacentNumbers(Symbol symbol, IEnumerable<Number> numbers) => numbers.Where(n => IsAdjacent(symbol, n));
+
+    public static IEnumerable<Symbol> GetSymbols(int line,  string input)
+        => SymbolRegex.Matches(input).Select(m => new Symbol(line, m.Index, m.Value[0]));
+
+    public static IEnumerable<Number> GetNumbers(int line, string input)
+        => NumberRegex.Matches(input).Select(m => new Number(line, m.Index, m.Length, int.Parse(m.Value)));
+
+    [GeneratedRegex(@"([^0-9\.])")]
+    private static partial Regex GetSymbolRegex();
+
+    [GeneratedRegex(@"(\d+)")]
+    private static partial Regex GetNumberRegex();
+}
