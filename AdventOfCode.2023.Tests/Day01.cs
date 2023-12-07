d928e1cf17b60f0ecfed19ef5f07f83058b3e89b
karalabe <karalabenak@gmail.com>
2023-12-07 21:39:00 +0100

    Solution for day 01

---
+++ AdventOfCode.2023.Tests/Day01.cs
@@ -0,0 +1,72 @@
+﻿namespace AdventOfCode2023.Tests;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.IO;
+
+[TestClass]
+public class Day01
+{
+    static readonly string TestInput = File.ReadAllText("Inputs/Day01.Example.txt");
+    static readonly string RealInput = File.ReadAllText("Inputs/Day01.txt");
+
+    [TestMethod]
+    public void TestInputLine1()
+    {
+        var input = TestInput.Split("\n")[0];
+
+        var result = AdventOfCode2023.Day01.Solve(input);
+
+        Assert.AreEqual("12", result);
+    }
+
+    [TestMethod]
+    public void TestInputLine2()
+    {
+        var input = TestInput.Split("\n")[1];
+
+        var result = AdventOfCode2023.Day01.Solve(input);
+
+        Assert.AreEqual("38", result);
+    }
+
+    [TestMethod]
+    public void TestInputLine3()
+    {
+        var input = TestInput.Split("\n")[2];
+
+        var result = AdventOfCode2023.Day01.Solve(input);
+
+        Assert.AreEqual("15", result);
+    }
+
+    [TestMethod]
+    public void TestInputLine4()
+    {
+        var input = TestInput.Split("\n")[3];
+
+        var result = AdventOfCode2023.Day01.Solve(input);
+
+        Assert.AreEqual("77", result);
+    }
+
+    [TestMethod]
+    public void WholeTestInput()
+    {
+        var input = TestInput;
+
+        var result = AdventOfCode2023.Day01.Solve(input);
+
+        Assert.AreEqual("142", result);
+    }
+
+    [TestMethod]
+    public void WholeRealInput()
+    {
+        var input = RealInput;
+
+        var result = AdventOfCode2023.Day01.Solve(input);
+
+        Assert.AreEqual("56506", result);
+    }
+}
---
+++ AdventOfCode.2023/Day01.cs
@@ -0,0 +1,25 @@
+﻿namespace AdventOfCode2023;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public partial class Day01
+{
+    private static Regex DigitRegex = MyRegex();
+
+    public static string? Solve(string input)
+        => input.Split("\n")
+            .Select(line => int.Parse(string.Join("", Filter(DigitRegex.Matches(line).Select(m => m.ToString())))))
+            .Sum()
+            .ToString();
+
+    public static IEnumerable<string> Filter(IEnumerable<string> source)
+    {
+        yield return source.First();
+        yield return source.Last();
+    }
+
+    [GeneratedRegex("[0-9]")]
+    private static partial Regex MyRegex();
+}
