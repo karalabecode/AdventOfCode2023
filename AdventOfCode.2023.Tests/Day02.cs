feaf96c30e90c62811cb8418b0aba5b3ad4ed898
karalabe <karalabenak@gmail.com>
2023-12-07 22:13:32 +0100

    Solution for day 02

---
+++ AdventOfCode.2023.Tests/Day02.cs
@@ -0,0 +1,62 @@
+﻿namespace AdventOfCode2023.Tests;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.IO;
+
+[TestClass]
+public class Day02
+{
+    static readonly string TestInput = File.ReadAllText("Inputs/Day02.Example.txt");
+    static readonly string RealInput = File.ReadAllText("Inputs/Day02.txt");
+
+    [TestMethod]
+    public void SolvePart1_ForFirstLine_ReturnsCorrectResult()
+    {
+        var input = TestInput.Split("\n")[0];
+
+        var result = AdventOfCode2023.Day02.SolvePart1(input);
+
+        Assert.AreEqual("1", result);
+    }
+
+    [TestMethod]
+    public void SolvePart1_ForWholeTestInput_ReturnsCorrectResult()
+    {
+        var input = TestInput;
+
+        var result = AdventOfCode2023.Day02.SolvePart1(input);
+
+        Assert.AreEqual("8", result);
+    }
+
+    [TestMethod]
+    public void SolvePart1_ForWholeRealInput_ReturnsCorrectResult()
+    {
+        var input = RealInput;
+
+        var result = AdventOfCode2023.Day02.SolvePart1(input);
+
+        Assert.AreEqual("1853", result);
+    }
+
+    [TestMethod]
+    public void SolvePart2_ForWholeTestInput_ReturnsCorrectResult()
+    {
+        var input = TestInput;
+
+        var result = AdventOfCode2023.Day02.SolvePart2(input);
+
+        Assert.AreEqual("2286", result);
+    }
+
+    [TestMethod]
+    public void SolvePart2_ForWholeRealInput_ReturnsCorrectResult()
+    {
+        var input = RealInput;
+
+        var result = AdventOfCode2023.Day02.SolvePart2(input);
+
+        Assert.AreEqual("72706", result);
+    }
+}
---
+++ AdventOfCode.2023/Day02.cs
@@ -0,0 +1,71 @@
+﻿namespace AdventOfCode2023;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+using Round = (int Red, int Green, int Blue);
+using Game = (int Id, System.Collections.Generic.IEnumerable<(int Red, int Green, int Blue)> Rounds);
+
+public partial class Day02
+{
+    private static Regex LineRegex = GetLineRegex();
+    
+    private static Regex RoundRegex = GetRoundRegex();
+
+    public static string? SolvePart1(string input)
+        => Parse(input)
+            .Where(g => g.Rounds.All(IsPossible))
+            .Select(g => g.Id)
+            .Sum()
+            .ToString();
+
+    public static string? SolvePart2(string input)
+        => Parse(input)
+            .Select(g => PowerOf(GetMinimumAmounts(g.Rounds)))
+            .Sum()
+            .ToString();
+
+    public static IEnumerable<Game> Parse(string input)
+        => input.Split("\n").Select(ParseLine);
+
+    public static Game ParseLine(string input)
+    {
+        var match = LineRegex.Match(input);
+        var id = int.Parse(match.Groups[1].Value);
+        var rounds = match.Groups[2].Captures.Select(c => ParseRound(c.Value)).ToList();
+
+        return (id, rounds);
+    }
+
+    public static bool IsPossible(Round round) => round.Red <= 12 && round.Green <= 13 && round.Blue <= 14;
+
+    public static Round GetMinimumAmounts(IEnumerable<Round> rounds)
+        => (
+            Red: rounds.Select(r => r.Red).Max(),
+            Green: rounds.Select(r => r.Green).Max(),
+            Blue: rounds.Select(r => r.Blue).Max()
+        );
+
+    public static int PowerOf(Round round) => round.Red * round.Green * round.Blue;
+
+    public static Round ParseRound(string input)
+    {
+        var match = RoundRegex.Match(input);
+        var groups = match.Groups[1].Captures
+            .Select(c => c.Value.Split(" "))
+            .ToDictionary(a => a[1], a => int.Parse(a[0]));
+        return (
+            Red: groups.TryGetValue("red", out var r) ? r : 0,
+            Green: groups.TryGetValue("green", out var g) ? g : 0,
+            Blue: groups.TryGetValue("blue", out var b) ? b : 0
+        );
+    }
+
+    [GeneratedRegex(@"Game (\d+): (?:([^;]+)(?:; )?)+")]
+    private static partial Regex GetLineRegex();
+
+    [GeneratedRegex(@"(?:(\d+ (?:red|green|blue))(?:, )?)+")]
+    private static partial Regex GetRoundRegex();
+}
