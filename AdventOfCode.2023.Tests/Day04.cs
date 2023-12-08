95e173ca2eabdaf04db6e8f21abd6a11aabbd067
karalabe <karalabenak@gmail.com>
2023-12-08 21:13:00 +0100

    Solution for day 04

---
+++ AdventOfCode.2023.Tests/Day04.cs
@@ -0,0 +1,53 @@
+﻿namespace AdventOfCode2023.Tests;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Collections.Generic;
+using System.IO;
+
+[TestClass]
+public class Day04
+{
+    static readonly IEnumerable<string> TestInput = File.ReadLines("Inputs/Day04.Example.txt");
+    static readonly IEnumerable<string> RealInput = File.ReadLines("Inputs/Day04.txt");
+
+    [TestMethod]
+    public void SolvePart1_ForWholeTestInput_ReturnsCorrectResult()
+    {
+        var input = TestInput;
+
+        var result = AdventOfCode2023.Day04.SolvePart1(input);
+
+        Assert.AreEqual("13", result);
+    }
+
+    [TestMethod]
+    public void SolvePart1_ForWholeRealInput_ReturnsCorrectResult()
+    {
+        var input = RealInput;
+
+        var result = AdventOfCode2023.Day04.SolvePart1(input);
+
+        Assert.AreEqual("27059", result);
+    }
+
+    [TestMethod]
+    public void SolvePart2_ForWholeTestInput_ReturnsCorrectResult()
+    {
+        var input = TestInput;
+
+        var result = AdventOfCode2023.Day04.SolvePart2(input);
+
+        Assert.AreEqual("30", result);
+    }
+
+    [TestMethod]
+    public void SolvePart2_ForWholeRealInput_ReturnsCorrectResult()
+    {
+        var input = RealInput;
+
+        var result = AdventOfCode2023.Day04.SolvePart2(input);
+
+        Assert.AreEqual("5744979", result);
+    }
+}
---
+++ AdventOfCode.2023/Day04.cs
@@ -0,0 +1,56 @@
+﻿namespace AdventOfCode2023;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public partial class Day04
+{
+    private static Regex NumberRegex = GetNumberRegex();
+
+    public static string? SolvePart1(IEnumerable<string> input)
+        => input.Sum(GetPoints).ToString();
+
+    public static string? SolvePart2(IEnumerable<string> input)
+        => input.Select((line, index) => (Index: index, Matches: GetMatchCount(line)))
+            .Aggregate(new Dictionary<int, int>(), UpdateCardCounts)
+            .Sum(p => p.Value)
+            .ToString();
+
+    private static Dictionary<int, int> UpdateCardCounts(Dictionary<int, int> cardCounts, (int Index, int Matches) card)
+    {
+        if (!cardCounts.TryGetValue(card.Index, out int value))
+        {
+            value = 1;
+            cardCounts[card.Index] = value;
+        }
+
+        var currentInstances = value;
+
+        for (var i = card.Index + 1; i < card.Index + card.Matches + 1; ++i)
+        {
+            cardCounts[i] = cardCounts.TryGetValue(i, out var r) ? r + currentInstances : 1 + currentInstances;
+        }
+
+        return cardCounts;
+    }
+
+    private static int GetPoints(string line)
+    {
+        var matches = GetMatchCount(line);
+
+        return matches > 0 ? (int)Math.Pow(2, matches - 1) : 0;
+    }
+
+    private static int GetMatchCount(string line)
+    {
+        var parts = line.Split(":")[1].Split("|");
+        var winning = NumberRegex.Matches(parts[0]).Select(m => m.Value).ToHashSet();
+        var mine = NumberRegex.Matches(parts[1]).Select(m => m.Value).ToList();
+        return mine.Count(winning.Contains);
+    }
+
+    [GeneratedRegex(@"(\d+)")]
+    private static partial Regex GetNumberRegex();
+}
