088eb8cddf968ec6a70345b82c58c2aab4f22fba
karalabe <karalabenak@gmail.com>
2023-12-10 00:25:02 +0100

    Solution for day 06

---
+++ AdventOfCode.2023.Tests/Day06.cs
@@ -0,0 +1,55 @@
+﻿namespace AdventOfCode2023.Tests;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Collections.Generic;
+using System.IO;
+
+using static AdventOfCode2023.Day06;
+
+[TestClass]
+public class Day06
+{
+    static readonly IEnumerable<string> TestInput = File.ReadLines("Inputs/Day06.Example.txt");
+    static readonly IEnumerable<string> RealInput = File.ReadLines("Inputs/Day06.txt");
+
+    [TestMethod]
+    public void SolvePart1_ForWholeTestInput_ReturnsCorrectResult()
+    {
+        var input = TestInput;
+
+        var result = SolvePart1(input);
+
+        Assert.AreEqual(288ul, result);
+    }
+
+    [TestMethod]
+    public void SolvePart1_ForWholeRealInput_ReturnsCorrectResult()
+    {
+        var input = RealInput;
+
+        var result = SolvePart1(input);
+
+        Assert.AreEqual(316800ul, result);
+    }
+
+    [TestMethod]
+    public void SolvePart2_ForWholeTestInput_ReturnsCorrectResult()
+    {
+        var input = TestInput;
+
+        var result = SolvePart2(input);
+
+        Assert.AreEqual(71503ul, result);
+    }
+
+    [TestMethod]
+    public void SolvePart2_ForWholeRealInput_ReturnsCorrectResult()
+    {
+        var input = RealInput;
+
+        var result = SolvePart2(input);
+
+        Assert.AreEqual(45647654ul, result);
+    }
+}
---
+++ AdventOfCode.2023/Day06.cs
@@ -0,0 +1,31 @@
+﻿namespace AdventOfCode2023;
+
+using System.Collections.Generic;
+using System.Linq;
+
+public partial class Day06 : Solution
+{
+    public static ulong? SolvePart1(IEnumerable<string> input) => Parse(input).Select(GetSolutionCount).Aggregate((a, b) => a * b);
+
+    public static ulong? SolvePart2(IEnumerable<string> input) => Parse(input.Select(l => l.Replace(" ", ""))).Select(GetSolutionCount).Aggregate((a, b) => a * b);
+
+    public static (ulong Time, ulong Length)[] Parse(IEnumerable<string> input)
+    {
+        var times = ParseNumbers(input.ElementAt(0), ulong.Parse).ToArray();
+        var lengths = ParseNumbers(input.ElementAt(1), ulong.Parse).ToArray();
+
+        return times.Zip(lengths).ToArray();
+    }
+
+    public static ulong GetSolutionCount((ulong Time, ulong Length) race)
+    {
+        var result = (ulong)0;
+
+        for (ulong i =  1; i < race.Time; i++)
+        {
+            if (race.Length < (race.Time - i) * i) result++;
+        }
+
+        return result;
+    }
+}
