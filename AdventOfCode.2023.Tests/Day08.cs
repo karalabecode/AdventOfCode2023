e453c5f2451f269e38acaf7efb22b6be7a9b7761
karalabe <karalabenak@gmail.com>
2023-12-10 20:48:40 +0100

    Solution for day 08

---
+++ AdventOfCode.2023.Tests/Day08.cs
@@ -0,0 +1,68 @@
+﻿namespace AdventOfCode2023.Tests;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+
+using static AdventOfCode2023.Day08;
+
+[TestClass]
+public class Day08
+{
+    static readonly IEnumerable<string> TestInput1 = File.ReadLines("Inputs/Day08.Example1.txt");
+    static readonly IEnumerable<string> TestInput2 = File.ReadLines("Inputs/Day08.Example2.txt");
+    static readonly IEnumerable<string> TestInput3 = File.ReadLines("Inputs/Day08.Example3.txt");
+    static readonly IEnumerable<string> RealInput = File.ReadLines("Inputs/Day08.txt");
+
+    [TestMethod]
+    public void SolvePart1_ForWholeTestInput1_ReturnsCorrectResult()
+    {
+        var input = TestInput1;
+
+        var result = SolvePart1(input);
+
+        Assert.AreEqual(2, result);
+    }
+
+    [TestMethod]
+    public void SolvePart1_ForWholeTestInput2_ReturnsCorrectResult()
+    {
+        var input = TestInput2;
+
+        var result = SolvePart1(input);
+
+        Assert.AreEqual(6, result);
+    }
+
+    [TestMethod]
+    public void SolvePart1_ForWholeRealInput_ReturnsCorrectResult()
+    {
+        var input = RealInput;
+
+        var result = SolvePart1(input);
+
+        Assert.AreEqual(14257, result);
+    }
+
+    [TestMethod]
+    public void SolvePart2_ForWholeTestInput3_ReturnsCorrectResult()
+    {
+        var input = TestInput3;
+
+        var result = SolvePart2(input);
+
+        Assert.AreEqual(6, result);
+    }
+
+    [TestMethod]
+    public void SolvePart2_ForWholeRealInput_ReturnsCorrectResult()
+    {
+        var input = RealInput;
+
+        var result = SolvePart2(input);
+
+        Assert.AreEqual(BigInteger.Parse("16187743689077"), result);
+    }
+}
---
+++ AdventOfCode.2023/Day08.cs
@@ -0,0 +1,98 @@
+﻿namespace AdventOfCode2023;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+using Big = System.Numerics.BigInteger;
+
+public partial class Day08 : Solution
+{
+    public static int? SolvePart1(IEnumerable<string> input)
+    {
+        var steps = input.First();
+
+        var map = input.Skip(2).Select(ParseTransition).ToDictionary(t => t.Item1, t => (t.Item2, t.Item3));
+
+        var s = 0;
+        var act = "AAA";
+
+        while (act != "ZZZ")
+        {
+            for (var i = 0; i < steps.Length && act != "ZZZ"; ++i)
+            {
+                act = steps[i] == 'L' ? map[act].Item1 : map[act].Item2;
+                ++s;
+            }
+        }
+
+        return s;
+    }
+
+    static Big GCF(Big a, Big b)
+    {
+        while (b != 0)
+        {
+            var temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    static Big LCM(Big a, Big b)
+    {
+        return (a / GCF(a, b)) * b;
+    }
+
+    public static Big? SolvePart2(IEnumerable<string> input)
+    {
+        var steps = input.First();
+
+        var map = input.Skip(2).Select(ParseTransition).ToDictionary(t => t.Item1, t => (t.Item2, t.Item3));
+
+        var s = 0;
+        var starting = map.Keys.Where(k => k.EndsWith('A')).ToList();
+        var current = map.Keys.Where(k => k.EndsWith('A')).ToList();
+
+        var filled = 0;
+        var touchMap = new int[starting.Count];
+
+        while (filled != starting.Count)
+        {
+            for (var i = 0; i < steps.Length; ++i)
+            {
+                ++s;
+
+                for (var j = 0; j < current.Count; ++j)
+                {
+                    var act = current[j];
+
+                    act = steps[i] == 'L' ? map[act].Item1 : map[act].Item2;
+
+                    current[j] = act;
+
+                    if (act.EndsWith('Z') && touchMap[j] == 0)
+                    {
+                        touchMap[j] = s;
+                        filled++;
+                    }
+                }
+            }
+        }
+
+        return touchMap.Select(i => (Big)i).Aggregate((Big)1, LCM);
+    }
+
+    private static (string, string, string) ParseTransition(string line)
+    {
+        var groups = LineRegex.Match(line).Groups;
+
+        return (groups[1].Value, groups[2].Value, groups[3].Value);
+    }
+
+    private static Regex LineRegex = GetLineRegex();
+
+    [GeneratedRegex(@"(.{3}) = \((.{3}), (.{3})\)")]
+    private static partial Regex GetLineRegex();
+}
